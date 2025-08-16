7f78f5607d0b960b1c2250a404fca680a74110dc
ProCoder217 <ottfreefree@gmail.com>
2025-08-16 15:15:36 +0530

    Update summerizer.py

--- summerizer.py
+++ summerizer.py
@@ -11,7 +11,7 @@ import threading
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
-
+nltk.download('punkt'); nltk.download('stopwords')"
 # scikit-learn
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
